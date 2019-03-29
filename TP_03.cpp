090ebf569a3ec46edfdbe3453109401433aafe5a
FacuCR <facucastro302@gmail.com>
2019-03-29 00:50:18 -0300

    Funcion cargar y mostrar datos terminado

--- TP_03.cpp
+++ TP_03.cpp
@@ -1,12 +1,14 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include <time.h>
 
 enum TRaza{Orco = 1, Humano, Mago, Enano, Elfo};
-char Nombres[10][10]={'Rock', 'G', 'C', 'D', 'P'};
-char Apellidos[10][10]={'Azul', 'V', 'S', 'D', 'B'};
+char Nombres[6][30]={"Rocket", "Gato", "Chubaca", "Pikachu", "Perro", "Capitan"};
+char Apellidos[6][30]={" Cansado", " Pro", " Loco", " Malo", " Traumado", " Bueno"};
 
 struct TDatos {
-	TRaza Raza; //nota 1
+	enum TRaza Raza; //nota 1
 	char *ApellidoNombre; //nota 2
 	int edad; //entre 0 a 300
 	double Salud;//100
@@ -23,28 +25,41 @@ struct TPersonaje {
 	TCaracteristicas * Caracteristicas;
 };
 
-void Cargar_DatPj(struct TDatos);
-void Mostrar_DatPj(struct TDatos);
+void Cargar_DatPj(struct TDatos *);
+void Mostrar_DatPj(struct TDatos *);
 
 int main()
 {
-	struct TDatos pj1;
-	Cargar_DatPj(pj1);
-	Mostrar_DatPj(pj1);
+	srand(time(NULL));
+	struct TDatos Dat, *pDat;
+	pDat = (TDatos *) malloc(sizeof(TDatos));
+	pDat = &Dat;
+
+
+	Cargar_DatPj(pDat);
+	Mostrar_DatPj(pDat);
 	
 	return 0;
 }
 
-void Cargar_DatPj(struct TDatos Personaje){
-	srand(time(NULL));
-	Personaje.Raza = 1 + rand() % 5;
-	Personaje.ApellidoNombre
 
+//Funcion Cargar datos del personaje
+void Cargar_DatPj(struct TDatos *DatosPj){
+	int ale = 1 + rand() % (5-1);
+	DatosPj->Raza = ale;
+	ale = 0 + rand() % (5-0);
+	strcpy(DatosPj->ApellidoNombre, Nombres[ale]); //Copio la cadena de nombres aleatoriamente
+	ale = 0 + rand() % (5-0);
+	strcat(DatosPj->ApellidoNombre, Apellidos[ale]);//Junto la cadena anterior con un apellido aleatorio
+	ale = 0 + rand() % (300-0);
+	DatosPj->edad = ale;
+	DatosPj->Salud = 100;
 }
 
-void Mostrar_DatPj(struct TDatos Personaje){
-	printf("Raza: ");
-	switch(Personaje.Raza){
+//Funcion Mostrar datos del personaje
+void Mostrar_DatPj(struct TDatos *DatosPj){
+	printf("Datos del personaje:\nRaza: ");
+	switch((*DatosPj).Raza){
 		case 1: printf("Orco\n");
 		break;
 		case 2: printf("Humano\n");
@@ -55,5 +70,9 @@ void Mostrar_DatPj(struct TDatos Personaje){
 		break;
 		case 5: printf("Elfo\n");
 		break;
+		default: printf("Error\n");
 	}
+	printf("Nombre: %s\n", (*DatosPj).ApellidoNombre);
+	printf("Edad: %d\n", (*DatosPj).edad);
+	printf("Salud: %.3f\n", (*DatosPj).Salud);
 }
