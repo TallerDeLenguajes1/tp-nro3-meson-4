e6e9cadc08ec640b014370ecefa8427514d22e2c
Mario <mario.delabarra@outlook.com>
2019-03-31 17:23:08 -0300

    TP 3 Completo

--- TP_03.cpp
+++ TP_03.cpp
@@ -3,13 +3,14 @@
 #include <stdio.h>
 #include <string.h>
 #include <time.h>
+#define MAX 3
 
-enum TRaza{Orco = 1, Humano, Mago, Enano, Elfo};
+enum TRaza{Orco, Humano, Mago, Enano, Elfo};
 char Nombres[6][30]={"Rocket", "Gato", "Chubaca", "Pikachu", "Perro", "Capitan"};
 char Apellidos[6][30]={" Cansado", " Pro", " Loco", " Malo", " Traumado", " Bueno"};
 
 struct TDatos {
-	enum TRaza Raza; //nota 1
+	TRaza Raza; //nota 1
 	char *ApellidoNombre; //nota 2
 	int edad; //entre 0 a 300
 	double Salud;//100
@@ -31,57 +32,95 @@ struct TPersonaje {
 };
 typedef struct TPersonaje TPersonaje;
 
+int Menu();
 void Cargar_DatPj(struct TDatos *);
 void Mostrar_DatPj(struct TDatos *);
 void CargarCarac(TCaracteristicas *puntCarac);
 void MostrarCarac(TCaracteristicas *puntCarac);
+void Pelea(TPersonaje *arrePjs, int primerPJ, int segPJ);
 
 int main(void)
 {	
 	srand(time(NULL));
-	struct TDatos Dat, *pDat;
-	pDat = (TDatos *) malloc(sizeof(TDatos));
-	pDat = &Dat;
+	struct TDatos *pDat;
+	TCaracteristicas *puntCarac;
+	TPersonaje *arrePjs;
+	int i=0, num, segPJ;
 
-	TCaracteristicas Carac, *puntCarac;
+	arrePjs = (TPersonaje *)malloc(sizeof(TPersonaje)*MAX);
 
-	puntCarac = (TCaracteristicas *)malloc(sizeof(TCaracteristicas));
-	puntCarac = &Carac;
+	//-----Tarea 3----
+	while(i<3){
+		arrePjs[i].DatosPersonales = (TDatos *) malloc(sizeof(TDatos));
+		arrePjs[i].Caracteristicas = (TCaracteristicas *)malloc(sizeof(TCaracteristicas));
+		Cargar_DatPj(arrePjs[i].DatosPersonales);
+		CargarCarac(arrePjs[i].Caracteristicas);
+		i++;
+	}
 
-	Cargar_DatPj(pDat);
-	Mostrar_DatPj(pDat);
-	CargarCarac(puntCarac);
-	MostrarCarac(puntCarac);
-
-
+	do{
+		num = Menu();
+		switch(num){
+			case 1:
+				printf("\n- Por favor, ingrese el numero del personaje que desea ver: ");
+				scanf("%d", &i);
+				printf("\n|=====Personaje %d=====|\n\n", i);
+				Mostrar_DatPj(arrePjs[i-1].DatosPersonales);
+				MostrarCarac(arrePjs[i-1].Caracteristicas);
+				break;
+			case 2:
+				printf("Por favor, elija el primer personaje: ");
+				scanf("%d", &i);
+				printf("\nPor favor, elija el segundo personaje: ");
+				scanf("%d", &segPJ);
+				Pelea(arrePjs, i, segPJ);
+				break;
+		}
+	}while(num!=9);
+	//-----------------------------
 
 
 	return 0;
 }
 
+int Menu(){
+
+	int num;
+
+	printf("\n\n<<<<<< Menu >>>>>>\n\n");
+	printf("1 - Mostrar personaje.\n");
+	printf("2 - Comenzar pelea entre 2 personajes.\n");
+	printf("9 - Salir.\n");
+	printf("Por favor elija un numero del menu: ");
+	scanf("%d", &num);
+
+	return(num);
+}
+
+//Funcion cargar Caracteristicas
 void CargarCarac(TCaracteristicas *puntCarac){
 
 	int numAle;
 
-	numAle = rand()% 10;
+	numAle = 1 + rand()% 10;
 	(*puntCarac).velocidad= numAle;
 
-	numAle = rand()% 5;
+	numAle = 1 + rand()% 5;
 	(*puntCarac).destreza= numAle;
 
-	numAle = rand()% 10;
+	numAle = 1+ rand()% 10;
 	(*puntCarac).fuerza = numAle;
 
-	numAle = rand()% 10;
+	numAle = 1 +rand()% 10;
 	(*puntCarac).Nivel = numAle;
 
-	numAle = rand()% 10;
+	numAle = 1 + rand()% 10;
 	(*puntCarac).Armadura = numAle;
 
 }
-
+//Funcion mostrar Caracteristicas
 void MostrarCarac(TCaracteristicas *puntCarac){
-	printf("Caracteristicas del poder de tu personaje: \n\n");
+	printf("|=====Caracteristicas del poder de tu personaje=====|\n\n");
 	printf("La velocidad del personaje es: %d.\n", (*puntCarac).velocidad);
 	printf("Su puntaje de destreza es: %d.\n", (*puntCarac).destreza);
 	printf("Tiene una fuerza de: %d de poder.\n", (*puntCarac).fuerza);
@@ -90,20 +129,37 @@ void MostrarCarac(TCaracteristicas *puntCarac){
 }
 //Funcion Cargar datos del personaje
 void Cargar_DatPj(struct TDatos *DatosPj){
-	int ale = 1 + rand() % (5-1);
-	DatosPj->Raza = ale;
-	ale = 0 + rand() % (5-0);
-	strcpy(DatosPj->ApellidoNombre, Nombres[ale]); //Copio la cadena de nombres aleatoriamente
-	ale = 0 + rand() % (5-0);
-	strcat(DatosPj->ApellidoNombre, Apellidos[ale]);//Junto la cadena anterior con un apellido aleatorio
-	ale = 0 + rand() % (300-0);
-	DatosPj->edad = ale;
+	int numAle = 1 + rand() % (5-1);
+	DatosPj->ApellidoNombre = (char * )malloc(sizeof(char *));
+	switch(numAle){
+		case 0:
+		DatosPj->Raza = Orco;
+		break;
+		case 1:
+		DatosPj->Raza = Humano;
+		break;
+		case 2:
+		DatosPj->Raza = Mago;
+		break;
+		case 3:
+		DatosPj->Raza = Enano;
+		break;
+		case 4:
+		DatosPj->Raza = Elfo;
+		break;
+	}
+	numAle = 0 + rand() % (5-0);
+	strcpy(DatosPj->ApellidoNombre, Nombres[numAle]); //Copio la cadena de nombres aleatoriamente
+	numAle = 0 + rand() % (5-0);
+	strcat(DatosPj->ApellidoNombre, Apellidos[numAle]);//Junto la cadena anterior con un apellido aleatorio
+	numAle = 0 + rand() % (300-0);
+	DatosPj->edad = numAle;
 	DatosPj->Salud = 100;
 }
 
 //Funcion Mostrar datos del personaje
 void Mostrar_DatPj(struct TDatos *DatosPj){
-	printf("Datos del personaje:\n\nRaza: ");
+	printf("|=====Datos del personaje=====|\n\nRaza: ");
 	switch((*DatosPj).Raza){
 		case 1: printf("Orco\n");
 		break;
@@ -121,3 +177,45 @@ void Mostrar_DatPj(struct TDatos *DatosPj){
 	printf("Edad: %d\n", (*DatosPj).edad);
 	printf("Salud: %.3f\n\n", (*DatosPj).Salud);
 }
+
+void Pelea(TPersonaje *arrePjs, int primerPJ, int segPJ){
+
+	int i=0;
+	int PD, ED, VA, PDEF, MDP=50000, danioProv;
+
+	printf("Bienvenidos a la zona de Pelea.\n");
+	while(i<3){
+		printf("|-------- Ronda %d -------|\n", i);
+		//Efectividad de ataque Personaje 1
+		PD = ((arrePjs[primerPJ-1].Caracteristicas->destreza)*(arrePjs[primerPJ-1].Caracteristicas->fuerza)*(arrePjs[primerPJ-1].Caracteristicas->Nivel));
+		ED = 1 + rand()% 100;
+		VA = PD * ED;
+		PDEF = ((arrePjs[primerPJ-1].Caracteristicas->Armadura)*(arrePjs[primerPJ-1].Caracteristicas->velocidad));
+		danioProv = (((VA*ED)-PDEF)/MDP)*100;
+		arrePjs[primerPJ-1].DatosPersonales->Salud = (arrePjs[primerPJ-1].DatosPersonales->Salud) - danioProv;
+		printf("El danio provocado al segundo personaje fue de: %d\n\n", danioProv);
+		//---------------------------------
+		//Efectividad de ataque Personaje 2
+		PD = ((arrePjs[segPJ-1].Caracteristicas->destreza)*(arrePjs[segPJ-1].Caracteristicas->fuerza)*(arrePjs[segPJ-1].Caracteristicas->Nivel));
+		ED = 1 + rand()% 100;
+		VA = PD * ED;
+		PDEF = ((arrePjs[segPJ-1].Caracteristicas->Armadura)*(arrePjs[segPJ-1].Caracteristicas->velocidad));
+		danioProv = (((VA*ED)-PDEF)/MDP)*100;
+		arrePjs[segPJ-1].DatosPersonales->Salud = (arrePjs[segPJ-1].DatosPersonales->Salud) - danioProv;
+		printf("El danio provocado al primer personaje fue de: %d\n\n", danioProv);
+		//---------------------------------
+		i++;
+	}
+				
+	printf("La salud del primer personaje es de: %.3f\n", arrePjs[primerPJ-1].DatosPersonales->Salud);
+	printf("La salud del segundo personaje es de: %.3f\n", arrePjs[segPJ-1].DatosPersonales->Salud);
+	if((arrePjs[primerPJ-1].DatosPersonales->Salud)>(arrePjs[segPJ-1].DatosPersonales->Salud)){
+		printf("Primer personaje gana.\n");
+	}else if((arrePjs[primerPJ-1].DatosPersonales->Salud)<(arrePjs[segPJ-1].DatosPersonales->Salud)){
+		printf("Segundo personaje gana.\n");
+	}else if((arrePjs[primerPJ-1].DatosPersonales->Salud)==(arrePjs[segPJ-1].DatosPersonales->Salud)){
+		printf("Empate entre ambos.\n");
+	}
+}
+
+
